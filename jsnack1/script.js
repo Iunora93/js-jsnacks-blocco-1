1a4785161a9ad5cfdb624dd83503e805a1c1c835
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-26 11:02:13 +0200

    Esercizio terminato

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,3 +2,20 @@
 
 /* L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga. */
+
+const firstWord = prompt("Scrivi la prima parola");
+console.log(firstWord);
+
+const secondWord = prompt("scrivi la seconda parola");
+console.log(secondWord);
+
+if ( firstWord > secondWord) {
+    alert("la prima parola è più lunga")
+    console.log(firstWord)
+} else if (firstWord < secondWord) {
+    alert("la seconda parola è più lunga")
+    console.log(secondWord)
+} else {
+    alert("le parole sono della stessa lunghezza")
+    console.log(firstWord, secondWord)
+}
