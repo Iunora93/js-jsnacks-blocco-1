9982bdf0599fbcae659a2eef6a83549e0c3886ba
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-26 12:13:10 +0200

    jsnack3

--- jsnack1/jsnack3/script.js
+++ jsnack1/jsnack3/script.js
@@ -1,1 +1,16 @@
 `use strict`
+
+const listName = ["eleonora", "Edoardo", "Guenda", "Macho"];
+
+const checkName = prompt("inserisci il tuo nome");
+console.log(checkName);
+
+for (let i = 0; i < listName.length; i++) {
+    checkName === listName[i];
+    if( checkName === listName ) {
+        alert("hai l'accesso")
+    } else {
+        alert("non hai l'accesso")
+    }
+} 
+
