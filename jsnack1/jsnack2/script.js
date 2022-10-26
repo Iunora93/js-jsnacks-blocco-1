3977d08745b798d7a9dd5ea4ce558e3f24449b47
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-26 11:42:31 +0200

    Somma numeri inseriti

--- jsnack1/jsnack2/script.js
+++ jsnack1/jsnack2/script.js
@@ -2,3 +2,15 @@
 
 /* Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
+
+const number = Number(prompt("inserisci un numero"));
+console.log(number);
+
+const numberS = Number(prompt("inserisci un numero"));
+console.log(numberS);
+
+const lastNumber = Number(prompt("inserisci un numero"));
+console.log(lastNumber);
+
+let result = number + numberS + lastNumber;
+console.log(result);
