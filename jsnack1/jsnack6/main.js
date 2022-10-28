6af7c94950e7ad2013670f0c58783c4f7de0bd44
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-28 10:49:38 +0200

    Listnumber [i]

--- jsnack1/jsnack6/main.js
+++ jsnack1/jsnack6/main.js
@@ -1,1 +1,19 @@
 `use strict`
+
+/* Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari */
+
+const listNumber = [1, 6, 4, 8, 10];
+console.log(listNumber);
+
+i = 0;
+let sum = 0;
+
+while( i <listNumber.length){
+    if(i % 2 == 1){
+        sum = sum + listNumber[i];
+    }
+    i++;
+}
+console.log(`${sum}`);
+
+
